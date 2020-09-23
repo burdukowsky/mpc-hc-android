0e7a788600a28b9a5d27b7a62f38b3e54c16189b
burdukowsky <burdukowskystas@gmail.com>
2020-09-23 18:01:17 +0700

    hosts & currentHostIndex in AppPreferences

--- app/src/main/java/tk/burdukowsky/mpc_hc_android/AppPreferences.kt
+++ app/src/main/java/tk/burdukowsky/mpc_hc_android/AppPreferences.kt
@@ -7,6 +7,10 @@ object AppPreferences {
 
     private const val PREFERENCES_NAME = "appPreferences"
     private const val LAYOUT_KEY = "layout"
+    private const val HOSTS_KEY = "hosts"
+    private const val HOSTS_DELIMITER = ","
+    private const val CURRENT_HOST_INDEX_KEY = "currentHostIndex"
+    private const val CURRENT_HOST_UNDEFINED = -1
 
     private val preferences: SharedPreferences =
         App.instance.getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE)
@@ -20,6 +24,27 @@ object AppPreferences {
     fun setLayout(layout: Layout) {
         val editor = preferences.edit()
         editor.putString(LAYOUT_KEY, layout.name)
+        editor.apply()
+    }
+
+    fun getHosts(): List<String> {
+        val hosts = preferences.getString(HOSTS_KEY, "") ?: ""
+        return hosts.split(HOSTS_DELIMITER)
+    }
+
+    fun setHosts(hosts: List<String>) {
+        val editor = preferences.edit()
+        editor.putString(HOSTS_KEY, hosts.joinToString(separator = HOSTS_DELIMITER))
+        editor.apply()
+    }
+
+    fun getCurrentHostIndex(): Int {
+        return preferences.getInt(CURRENT_HOST_INDEX_KEY, CURRENT_HOST_UNDEFINED)
+    }
+
+    fun setCurrentHostIndex(index: Int) {
+        val editor = preferences.edit()
+        editor.putInt(CURRENT_HOST_INDEX_KEY, index)
         editor.apply()
     }
 
