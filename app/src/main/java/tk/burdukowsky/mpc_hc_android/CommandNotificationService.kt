ecc085e553baf7be9914f6685b28cf3b4242b430
Stanislav Burdukowsky <burdukowskystas@gmail.com>
2022-10-01 02:03:49 +0700

    fix notification button clicks

--- app/src/main/java/tk/burdukowsky/mpc_hc_android/CommandNotificationService.kt
+++ app/src/main/java/tk/burdukowsky/mpc_hc_android/CommandNotificationService.kt
@@ -1,14 +1,15 @@
 package tk.burdukowsky.mpc_hc_android
 
-import android.app.Notification
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 
 class CommandNotificationService : Service() {
@@ -16,14 +17,21 @@ class CommandNotificationService : Service() {
     class CommandInfo(val resourceId: Int, val requestCode: Int)
 
     private val notificationId = 42
-    private val notificationChannelId = "commandNotificationChannelId"
+    private val notificationChannelIdValue = "commandNotificationChannelId"
+    private lateinit var notificationChannelId: String
     private val stopServiceReceiverIntentFilterAction = "stopServiceReceiver"
+    private val commandReceiverIntentFilterAction = "commandReceiver"
+    private val commandReceiverIntentExtra = "command"
     private val commandInfoMap: Map<Command, CommandInfo> = mapOf(
         Command.play to CommandInfo(R.id.notification_play, 0),
         Command.pause to CommandInfo(R.id.notification_pause, 1),
         Command.volume_down to CommandInfo(R.id.notification_volume_down, 2),
         Command.volume_up to CommandInfo(R.id.notification_volume_up, 3)
     )
+
+    override fun onCreate() {
+        notificationChannelId = getNotificationChannelId()
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -35,12 +43,19 @@ class CommandNotificationService : Service() {
     }
 
     override fun onDestroy() {
-        stopForeground(true)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            stopForeground(STOP_FOREGROUND_DETACH)
+        } else {
+            stopForeground(true)
+        }
+        unregisterReceiver(stopServiceReceiver)
+        unregisterReceiver(commandReceiver)
         super.onDestroy()
     }
 
     private fun start() {
         registerReceiver(stopServiceReceiver, IntentFilter(stopServiceReceiverIntentFilterAction))
+        registerReceiver(commandReceiver, IntentFilter(commandReceiverIntentFilterAction))
 
         val remoteViews = RemoteViews(packageName, R.layout.notification)
 
@@ -58,10 +73,10 @@ class CommandNotificationService : Service() {
             remoteViews.setOnClickPendingIntent(
                 commandInfo.resourceId,
                 PendingIntent.getBroadcast(
-                    applicationContext,
+                    this,
                     commandInfo.requestCode,
-                    Intent("tk.burdukowsky.mpc_hc_android.NOTIFICATION_COMMAND")
-                        .apply { putExtra(CommandReceiver.intentExtra, command.value) },
+                    Intent(commandReceiverIntentFilterAction)
+                        .apply { putExtra(commandReceiverIntentExtra, command.value) },
                     PendingIntent.FLAG_UPDATE_CURRENT
                 )
             )
@@ -82,6 +97,42 @@ class CommandNotificationService : Service() {
         override fun onReceive(context: Context?, intent: Intent?) {
             stopSelf()
         }
+    }
+
+    private var commandReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                DoAsync {
+                    try {
+                        CommandService.send(intent.getIntExtra(commandReceiverIntentExtra, 0))
+                    } catch (t: Throwable) {
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getNotificationChannelId(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannelAndGetId()
+        } else {
+            notificationChannelIdValue
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannelAndGetId(): String {
+        val channel =
+            NotificationChannel(
+                notificationChannelIdValue,
+                "MPC-HC Remote Control Service",
+                NotificationManager.IMPORTANCE_NONE
+            )
+        channel.lightColor = Color.DKGRAY
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return notificationChannelIdValue
     }
 
 }
